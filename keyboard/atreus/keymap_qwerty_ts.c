d4fecf806eb05a02e64bf83f2e59c1e6c102a0f8
Tom Short <tshort.rlists@gmail.com>
2015-05-17 08:35:01 -0400

    keymap changes

--- keyboard/atreus/keymap_qwerty_ts.c
+++ keyboard/atreus/keymap_qwerty_ts.c
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          
 
   /* 2: numbers and arrows/movement -- "sticky toggled" with FN1 */
-  KEYMAP(SHIFT(KC_EQUAL), KC_1, KC_2, KC_3, KC_MINS,   KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, \
-         SHIFT(KC_8),     KC_4, KC_5, KC_6, KC_SLSH,   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_NO, \
-         KC_EQUAL,        KC_7, KC_8, KC_9, KC_DOT,    KC_NO, KC_NO, KC_NO, KC_NO, KC_FN7, \
-         KC_PGDN, KC_PGUP, KC_FN6, KC_FN1, KC_BSPC, KC_FN3, KC_FN4,     \
+  KEYMAP(SHIFT(KC_EQUAL), KC_7, KC_8, KC_9, KC_MINS,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_NO, \
+         SHIFT(KC_8),     KC_4, KC_5, KC_6, KC_SLSH,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_NO, \
+         KC_EQUAL,        KC_1, KC_2, KC_3, KC_DOT,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,  KC_FN7, \
+         KC_0, KC_0, KC_FN6, KC_FN1, KC_BSPC, KC_FN3, KC_FN4,     \
          KC_SPC, KC_FN0, KC_FN5, KC_QUOT, KC_ENT)
   
 };
@@ -35,7 +35,7 @@ const uint16_t PROGMEM fn_actions[] = {
   [3] = ACTION_MODS_ONESHOT(MOD_LSFT),
   [4] = ACTION_MODS_TAP(MOD_LCTL, KC_ESC),
   [5] = ACTION_MODS_TAP(MOD_LALT, KC_MINS),
-  [6] = ACTION_MODS_TAP(MOD_LGUI, KC_0),
+  [6] = ACTION_MODS_TAP(MOD_LGUI, KC_DOT ),
   [7] = ACTION_FUNCTION(BOOTLOADER)
 };
 
