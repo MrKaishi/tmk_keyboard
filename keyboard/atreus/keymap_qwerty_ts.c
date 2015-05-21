e4029034a1cae92e2e07c712fbc08daf3229cbe3
Tom Short <tshort.rlists@gmail.com>
2015-05-21 11:33:20 -0400

    adjust numerics

--- keyboard/atreus/keymap_qwerty_ts.c
+++ keyboard/atreus/keymap_qwerty_ts.c
@@ -5,7 +5,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
          KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
          KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-         KC_GRAVE, KC_EQUAL, KC_FN2, KC_FN1, KC_FN3, KC_FN4, KC_LALT,     \
+         KC_EQUAL, KC_GRAVE, KC_FN2, KC_FN1, KC_FN3, KC_FN4, KC_LALT,     \
          KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_ENT),                     \
 
   /* 1: punctuation and fn keys -- momentarily toggled with FN0 */
@@ -15,16 +15,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_BSLS, KC_F5, KC_F6, KC_F7, KC_F8,       \
          KC_EQUAL, SHIFT(KC_5), KC_LBRC, KC_RBRC, SHIFT(KC_6), \
          SHIFT(KC_7), KC_F9, KC_F10, KC_F11, KC_F12,       \
-         KC_GRAVE, KC_EQUAL, KC_FN2, KC_FN1, KC_FN3, KC_FN4, KC_LALT,     \
-         KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_ENT),                     \
+         KC_EQUAL, KC_GRAVE, KC_FN2, KC_FN1, KC_FN3, KC_FN4, KC_LALT,     \
+         KC_SPC, KC_FN0, KC_DEL, KC_QUOT, KC_ENT),                     \
          
 
   /* 2: numbers and arrows/movement -- "sticky toggled" with FN1 */
   KEYMAP(SHIFT(KC_EQUAL), KC_7, KC_8, KC_9, KC_MINS,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_NO, \
          SHIFT(KC_8),     KC_4, KC_5, KC_6, KC_SLSH,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_NO, \
-         KC_EQUAL,        KC_1, KC_2, KC_3, KC_DOT,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,  KC_FN7, \
-         KC_0, KC_0, KC_FN6, KC_FN1, KC_FN3, KC_FN4, KC_LALT,     \
-         KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_ENT)
+         KC_0,            KC_1, KC_2, KC_3, KC_DOT,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,  KC_FN7, \
+         KC_EQUAL, KC_DOT, KC_FN2, KC_FN1, KC_FN3, KC_FN4, KC_LALT,     \
+         KC_SPC, KC_FN0, KC_DEL, KC_QUOT, KC_ENT)
   
 };
 
